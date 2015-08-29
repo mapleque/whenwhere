dfac56783a5e88a160555397ce4b961f23ca860b
mapleque <mapleque@163.com>
2015-08-29 17:16:51 +0800

    new model

--- README.md
+++ README.md
@@ -2,48 +2,40 @@
 ##概述
 这是一个私人项目，主要用于朋友间约会确定时间。主办者提出聚会邀请，参与者标注自己可参加的时间，最后由主办者确认时间举办一个聚会。
 ##功能列表
-* 主办者创建聚会
-* 主办者发出聚会邀请
-* 参与者圈定可参与时间
+* 主办者创建聚会、发出聚会邀请
+* 参与者圈定可参与时间、地点
 * 主办者敲定聚会
+* 用户查看自己的聚会列表，包括发起的和参与的
 
-##实现方式
+##实现
 ####View
+* 主页 (目前感觉没啥意义啊)
+	* 添加聚会入口，聚会列表入口
 * 聚会列表视图
-    * 项目主页、列出现有聚会，点击可查看聚会详情，提供创建聚会入口
-* 创建聚会视图
-    * 列出聚会内容、形式、可选择时间、地点等
+    * 列出发起的和参与的聚会，点击可查看聚会详情，提供创建聚会入口
 * 聚会详情视图
-    * 查看聚会内容,给主办者之外的用户提供参与聚会入口
-    * 如果是已经敲定的聚会，显示聚会详情及参与人员
+    * 主办者可以提交聚会内容、形式、可选择时间、地点等
+    * 参与者查看聚会内容,选择聚会地点和时间
+    * 如果是已经敲定的聚会，显示聚会详情及参与人员，所有内容不能再修改
     * 如果是未敲定聚会，主办者有权修改聚会内容
-* 参与聚会视图
-    * 添加可出席时间，地点建议，聚会方式建议等
 
 ####Model
-* 聚会
-	* pid、聚会主题、详情、可参与人数、创建时间、时间、地点
-* 用户
-	* uuid、昵称、联系方式
-* 参与意愿
-	* pid、聚会pid、用户uuid、出席时间、地点建议、其他建议
+@view www/js/model.js
+* Party
+* User
+* When
+* Where
 
 ####ViewModel
-* list-party
-	* get party list
+* list
+	* get host party list
 	* show the list
-* create-party
-	* properties input
-	* create party
-* view-party
-	* get party model
-	* show party properties
-    * show attend entrence for attender
-    * show update entrence for hoster
-* attend-party
+	* get aattend party list
+	* show the list
+* party
+	* if view, get party model, show party properties
 	* input attend date
 	* input attend addr
-	* input other advice
-* update-party
-	* change party properties
-	* ensure party
+	* if crate, properties input, set party model
+	* if host, change party properties
+	* if host, ensure party
---
+++ www/js/model.js
@@ -0,0 +1,86 @@
+(function(){
+	'use strict';
+	/**
+	 * class base model
+	 * private
+	 * root class
+	 */
+	var BaseModelClass = function(){};
+	BaseModelClass.prototype.fromJson = function(json){//通过json给类属性赋值
+		if (typeof json !== 'object')
+			return;
+		for (var key in json){
+			this[key]=json[key];
+		}
+	},
+	BaseModelClass.prototype.toJsonStr = function(){//将当前类属性转成json
+		return JSON.stringify(this);
+	}
+	Date.prototype.toString =
+	Date.prototype.toJSON =
+	function (){
+		var str=[];
+		str.push(this.getFullYear());
+		str.push(this.getMonth()<9?'0'+(this.getMonth()+1):(this.getMonth()+1));
+		str.push(this.getDate());
+		return str.join('-');
+	}
+
+	var M = window.M = {};
+	/**
+	 * class party
+	 * public
+	 * extend base model class
+	 */
+	var Party = M.Party = function() {
+		this.pid = null;			//pid
+		this.title = null;			//标题
+		this.describe = null;		//详细说明
+		this.member = [];			//参与人,其中带有参与信息
+		this.createDate = null;		//创建时间
+		this.author = null;			//创建人
+		this.updateDate = null;		//最后修改时间
+		this.when = [];				//拟聚会日期
+		this.where = [];			//拟聚会地点
+	};
+	Party.prototype = new BaseModelClass();
+
+	/**
+	 * class user 
+	 * public
+	 * extend base model class
+	 */
+	var User = M.User = function(){
+		this.pid = null;			//pid
+		this.nickname = null;		//昵称
+		this.phone = null;			//电话
+		this.hostParty = [];		//发起的聚会列表
+		this.attendParty = [];		//参与的聚会列表
+	};
+	User.prototype = new BaseModelClass();
+
+	/**
+	 * class when
+	 * public
+	 * extend base model class
+	 */
+	var When = M.When = function(){
+		this.date = null;			//当前日期
+		this.selectMember = [];		//选择当前日期的参与人
+		this.isChoose = false;		//是否选择当前日期
+	};
+	When.prototype = new BaseModelClass();
+
+	/**
+	 * class when
+	 * public
+	 * extend base model class
+	 */
+	var Where = M.Where = function(){
+		this.addr = null;			//当前地点
+		this.selectMember = [];		//选择当前地点的参与人
+		this.isChoose = false;		//是否选择当前地点
+	};
+	Where.prototype = new BaseModelClass();
+
+})();
